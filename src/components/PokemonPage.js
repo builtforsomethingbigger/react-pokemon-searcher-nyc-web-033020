c497e1749a2562bb0db7af5d3336e034c7e2785d
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-06-11 09:21:42 -0400

    Added Dropdown Filtering for Speed-Defense-SpAttack

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -11,7 +11,8 @@ class PokemonPage extends React.Component {
   state = {
     pokemons: [],
     searchInput: '',
-    radioBtnValue: ''
+    radioBtnValue: '',
+    dropdownValue: ''
   }
 
   componentDidMount(){
@@ -61,6 +62,41 @@ class PokemonPage extends React.Component {
     }
   }
 
+  dropdownHandler = e => {
+    this.setState({
+      dropdownValue: e.target.value
+    })
+    const pokemons = [...this.state.pokemons]
+    if(e.target.value === 'speed'){
+      const speedSort = pokemons.sort((a,b) => {
+        let speedA = a.stats.find(stat => stat.name === 'speed')
+        let speedB = b.stats.find(stat => stat.name === 'speed')
+       return (speedA.value - speedB.value)
+      })
+      this.setState({
+        pokemons: speedSort
+      })
+    }else if(e.target.value === 'defense'){
+      const defenseSort = pokemons.sort((a,b) => {
+        let defenseA = a.stats.find(stat => stat.name === 'defense')
+        let defenseB = b.stats.find(stat => stat.name === 'defense')
+       return (defenseA.value - defenseB.value)
+      })
+      this.setState({
+        pokemons: defenseSort
+      })
+    }else if(e.target.value === 'special-attack'){
+      const specialAttackSort = pokemons.sort((a,b) => {
+        let specialAttackA = a.stats.find(stat => stat.name === 'special-attack')
+        let specialAttackB = b.stats.find(stat => stat.name === 'special-attack')
+       return (specialAttackA.value - specialAttackB.value)
+      })
+      this.setState({
+        pokemons: specialAttackSort
+      })
+    }
+  }
+
   render() {
   
     return (
@@ -71,8 +107,16 @@ class PokemonPage extends React.Component {
         <br />
         <Search onChange={this.searchPokemon} value={this.state.searchInput} />
           <label>Name A-Z</label> <input type="radio" value="A-Z" checked={this.state.radioBtnValue == "A-Z"} onChange={this.radioChangeHandler}/>
-          
+          <br/>
           <label>HP</label> <input type="radio" value="hp" checked={this.state.radioBtnValue == "hp"} onChange={this.radioChangeHandler}/>
+          <br/>
+          <select onChange={this.dropdownHandler}>
+            <option>Select a Filter</option>
+            <option name='speed' value='speed'>Speed</option>
+            <option name='defense' value='defense'>Defense</option>
+            <option name='special-attack' value='special-attack'>Special Attack</option>
+          </select>
+        <br />
         <br />
         <PokemonCollection pokemons={this.renderSearchedPokemon()} onDoubleClick={this.renderPokemon} />
       </Container>
